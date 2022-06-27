6c85eac53891cda43dab1d426423749b73917da8
Mide Dickson <dicksonmide@gmail.com>
2022-06-27 12:14:52 +0100

    improve auth view to match coding standard

--- authentication/views.py
+++ authentication/views.py
@@ -1,21 +1,17 @@
-from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, status, response, permissions
 from . import serializers
 from utils.custom_exception_handler import get_response
 # Create your views here.
 
 
-class UserCreateView(GenericAPIView):
+class UserCreateView(generics.GenericAPIView):
     serializer_class = serializers.UserSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (permissions.AllowAny, )
 
     def post(self, request):
         data = request.data
         serializer = self.serializer_class(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(get_response(message="User Created Successfully", result=serializer.data, success=True, status=HTTP_201_CREATED), status=HTTP_201_CREATED)
-        return Response(get_response(message="Input Validation Error", errors=serializer.errors, status=HTTP_400_BAD_REQUEST), status=HTTP_400_BAD_REQUEST)
+            return response.Response(get_response(message="User Created Successfully", result=serializer.data, success=True, status=status.HTTP_201_CREATED), status=status.HTTP_201_CREATED)
+        return response.Response(get_response(message="Input Validation Error", errors=serializer.errors, status=status.HTTP_400_BAD_REQUEST), status=status.TTP_400_BAD_REQUEST)
